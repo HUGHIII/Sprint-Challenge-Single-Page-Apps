b9defb8ceddc8768e196ee7c18de4c0028108554
TREY <hugh.a.lloyd@gmail.com>
2020-01-31 11:55:36 -0600

    displaying char info

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
+import { Route } from 'react-router-dom';
 import Header from "./components/Header.js";
+import CharacterList from './components/CharacterList';
+import LocationList from './components/LocationsList';
+import SearchForm from './components/SearchForm';
+import WelcomePage from './components/WelcomePage';
+
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <CharacterList/>
     </main>
   );
 }
+
+
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,15 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return (
+          <div>
+        <h2>{props.char.name}</h2>
+            <ul>Character Details</ul>
+            <li>{props.char.species}</li>
+            <li>{props.char.status}</li>
+        </div>
+    )
+  
+  
+        
 }
